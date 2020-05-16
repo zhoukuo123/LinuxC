0e6d79f313ce5aea23933e17b4edb7f0e00ef2cc
zhoukuo123 <1178053078@qq.com>
2020-05-16 15:32:48 +0800

    update

---
+++ 线程/pthread_cancel.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *th_fun(void *arg) {
+    int i = 0;
+    int *p = (int *)arg;
+    printf("thread PID = %d\n", getpid());
+    printf("thread ID = %lu\n", pthread_self());
+    printf("thread *arg = %d\n", *p);
+    while (1) {
+        i++;
+        pthread_testcancel();
+    }
+}
+
+int main() {
+    pthread_t tid;
+    int n = 10;
+
+    pthread_create(&tid, NULL, th_fun, (void *)&n);
+    printf("main thread ID = %lu\n", pthread_self());
+    pthread_cancel(tid);
+    while (1) {
+        sleep(1);
+    }
+    return 0;
+}
---
+++ 线程/pthread_detach.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *thr_fn(void *arg) {
+    int n = 3;
+    while (n--) {
+        printf("thread count %d\n", n);
+        sleep(1);
+    }
+    return (void *)1;
+}
+
+int main() {
+    pthread_t tid;
+    void *tret;
+    int err;
+
+    pthread_create(&tid, NULL, thr_fn, NULL);
+    pthread_detach(tid);
+
+    while (1) {
+        err = pthread_join(tid, &tret);
+        if (err != 0) {
+            fprintf(stderr, "thread %s\n", strerror(err));
+        } else {
+            fprintf(stdout, "thread exit code %d\n", (int)tret);
+        }
+        sleep(1);
+    }
+    return 0;
+}
