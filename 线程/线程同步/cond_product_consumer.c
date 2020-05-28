e1b2b11b93f522c73fb026814c75e1e1250c2b5d
zhoukuo123 <1178053078@qq.com>
2020-05-28 15:33:19 +0800

    update

--- 线程/线程同步/cond_product_consumer.c
+++ 线程/线程同步/cond_product_consumer.c
@@ -24,13 +24,37 @@ pthread_cond_t has_data = PTHREAD_COND_INITIALIZER;
 
 void *producer(void *arg) {
     struct msg *mp;
+    int n = 10;
+    while (n--) {
+        mp  = malloc(sizeof(struct msg));
+        mp->num = rand() % 1000 + 1;
+        printf("--produce %d\n", mp->num);
+        pthread_mutex_lock(&mutex);
+        mp->next = head;
+        head = mp;
+        pthread_mutex_unlock(&mutex);
+
+        pthread_cond_signal(&has_data); // 将阻塞在该条件变量上的一个线程唤醒
+        sleep(rand() % 3);
+    }
     return NULL;
 }
 
 void *consumer(void *arg) {
+    struct msg *mp;
     pthread_mutex_lock(&mutex); // 加锁
-    if (head == NULL) {
-        pthread_cond_wait(&has_data, &mutex); //  阻塞等待条件变量, 判断条件是否满足
+    int n = 10;
+    while (n--) {
+        if (head == NULL) {
+            pthread_cond_wait(&has_data, &mutex); //  阻塞等待条件变量, 判断条件是否满足
+        }
+
+        mp = head;
+        head = mp->next;
+        pthread_mutex_unlock(&mutex);
+        printf("--consume %d\n", mp->num);
+        free(mp);
+        sleep(rand() % 3);
     }
 
     return NULL;
@@ -39,6 +63,7 @@ void *consumer(void *arg) {
 int main() {
     pthread_t pid, cid;
     int ret;
+    srand(time(NULL));
     ret = pthread_create(&pid, NULL, producer, NULL);
     if (ret != 0) {
         err_thread(ret, "pthread_create producer error");
