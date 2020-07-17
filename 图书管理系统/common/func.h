82fd65b3d419d959ec6f71fd9a7117b30f0ed864
zhoukuo123 <1178053078@qq.com>
2020-07-17 15:05:17 +0800

    update

--- 图书管理系统/common/func.h
+++ 图书管理系统/common/func.h
@@ -1,8 +1,41 @@
-//
-// Created by linux on 2020/7/17.
-//
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mysql/mysql.h>
+#include "main.h"
 
-#ifndef LINUXC_FUNC_H
-#define LINUXC_FUNC_H
+#define _CRT_SECURE_NO_WARNINGS
 
-#endif //LINUXC_FUNC_H
+///////借阅者的函数功能部分///////
+//借书功能（先查询，再借书）
+int borrowBooks(char book_id[MAXLENGTH], char read_id[MAXLENGTH]);
+
+//还书---查询借阅情况+还书
+int checkBorrowed();
+int returnBooks(char book_id[MAXLENGTH], char reader_id[MAXLENGTH]);
+
+//查询个人信息
+int checkInfo();
+int updateInfo(int updateNum, char read_id[MAXLENGTH]);
+
+//申请续借
+int checkRenew();
+int renew(char book_id[MAXLENGTH], char reader_id[MAXLENGTH]);
+
+//交延期罚金
+int payDelayPenalty(char reader_id[MAXLENGTH]);
+
+/////////////管理员的函数功能部分//////////
+//查询个人信息
+int SecPi();
+
+//修改个人信息
+int ModifyPi();
+
+//查询图书信息
+int checkbook(int flag);
+
+
+
+
--- 图书管理系统/common/main.h
+++ 图书管理系统/common/main.h
@@ -1,8 +1,82 @@
-//
-// Created by linux on 2020/7/17.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <mysql/mysql.h>
+#include "../common/func.h"
+#include "../common/menu.h"
+#include "../common/login.h"
 
-#ifndef LINUXC_MAIN_H
-#define LINUXC_MAIN_H
+// 未知
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning( disable : 4996)
 
-#endif //LINUXC_MAIN_H
+#define MAXLENGTH 1024
+#define IP "localhost"
+#define PASSWORD "0000"
+
+MYSQL mysql;
+MYSQL_RES *result;
+MYSQL_ROW row;
+int rowcount;
+char ch[2];
+
+char login_id[MAXLENGTH];
+char login_password[MAXLENGTH];
+char login_name[MAXLENGTH];
+char login_gender[MAXLENGTH];
+char login_grade[MAXLENGTH];
+char login_phone[MAXLENGTH];
+char login_department[MAXLENGTH];
+char query[MAXLENGTH];
+
+int updateNum; // 更新用户信息的时候，用户选择的更新项
+char update[MAXLENGTH]; // 更改后的内容
+
+char checkBorrowed_ret[MAXLENGTH]; // 用于接收用户在看到借阅记录后的操作内容
+
+char school_id[MAXLENGTH];//校园卡账号
+char school_password[MAXLENGTH];//校园卡密码
+
+//查询图书所用变量
+char type1[MAXLENGTH];
+char bookclass[MAXLENGTH];
+char author[MAXLENGTH];
+char bookname[MAXLENGTH];
+char isbn[MAXLENGTH];
+char bookid[MAXLENGTH];
+char price[MAXLENGTH];
+char publish[MAXLENGTH];
+char location[MAXLENGTH];
+char info[MAXLENGTH];
+char writer[MAXLENGTH];
+double priceUser;//用户输入的价格
+int locationUser;//用户输入的书架位置
+
+//针对查询图书进行下一次操作的变量
+char book_id_next[MAXLENGTH];  //用户借书的
+
+//统计
+char type[MAXLENGTH];
+char type2[MAXLENGTH];
+char bookname1[MAXLENGTH];
+char readername[MAXLENGTH];
+char departmentid[MAXLENGTH];
+char sexid[MAXLENGTH];
+char borrowtimes[MAXLENGTH];
+char grade[MAXLENGTH];
+
+//查看罚金
+char fines[MAXLENGTH];
+char reader_id1[MAXLENGTH];
+char reader_id2[MAXLENGTH];
+char school_id1[MAXLENGTH];
+char school_password1[MAXLENGTH];
+char school_id2[MAXLENGTH];
+char school_password2[MAXLENGTH];
+char bookname2[MAXLENGTH];
+
+#define N 4
+
+
+
