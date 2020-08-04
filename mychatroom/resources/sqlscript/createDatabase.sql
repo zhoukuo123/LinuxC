50af076c3e7e745e8242b2fc7b497b9e13d73235
zhoukuo123 <1178053078@qq.com>
2020-08-04 22:29:54 +0800

    update

--- mychatroom/resources/sqlscript/createDatabase.sql
+++ mychatroom/resources/sqlscript/createDatabase.sql
@@ -2,33 +2,41 @@ drop database if exists mychatroom;
 create database if not exists mychatroom character set 'utf8';
 use mychatroom;
 
-create table user_data
+create table user_data # 用户数据表
 (
-    account     varchar(20) primary key,
+    account     varchar(20) not null primary key,
     nickname    varchar(20) not null,
     password    varchar(20) not null,
-    user_status int         not null,
+    user_status int         not null, # 用户状态(是否在线)
     user_socket int         not null
 );
 
-insert into user_data
-values ('1178053077', '123456');
-
-create table friends
+create table `groups` # 群信息表
 (
-    friends_id       varchar(20) primary key,
-    friends_nickname varchar(20) not null,
-    friends_status   int         not null,
-    friends_socket   int         not null,
+    group_account       varchar(20) not null primary key, # 群号
+    group_name          varchar(20) not null,             # 群名
+    group_member_number datetime    not null              # 群成员数量
 );
 
-insert into friends
-values ('abs', '123456', '李明', '1', '1', '18022334455', '1', 0, 20, 2, 0);
-
-create table group
+create table group_members # 群成员表
 (
-    group_account       varchar(20) not null,
-    group_name          varchar(20) not null,
-    group_member_number datetime    not null,
+    group_account         varchar(20) not null primary key, # 群号
+    group_name            varchar(20) not null,             # 群名
+    group_member_account  varchar(20) not null,             # 群成员账号
+    group_member_nickname varchar(20) not null,             # 群成员昵称
+    group_state           varchar(20) not null              # 群成员群地位(群主, 管理员, 普通群员)
 );
 
+create table friends # 好友表
+(
+    user        varchar(20) not null primary key, # 当前用户账号
+    friend_user varchar(20) not null,             # 该用户好友账号
+    realtion    int         not null              # 两人的关系(特别关心,黑名单,普通)
+);
+
+create table chat_messages # 聊天信息表
+(
+    send_user varchar(20) not null, # 发送者的账号
+    recv_user varchar(20) not null, # 接收者的账号
+    messages  varchar(30) not null  # 消息内容
+);
