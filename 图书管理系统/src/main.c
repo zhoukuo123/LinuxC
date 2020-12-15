5ff44d6d4a2ac6334943f99ac8ec0fba03a44e34
zhoukuo123 <1178053078@qq.com>
2020-12-15 18:37:03 +0800

    reformat code

--- 图书管理系统/src/main.c
+++ 图书管理系统/src/main.c
@@ -19,10 +19,10 @@ char update[MAXLENGTH]; // 更改后的内容
 
 char checkBorrowed_ret[MAXLENGTH]; // 用于接收用户在看到借阅记录后的操作内容
 
-char school_id[MAXLENGTH];//校园卡账号
-char school_password[MAXLENGTH];//校园卡密码
+char school_id[MAXLENGTH];// 校园卡账号
+char school_password[MAXLENGTH];// 校园卡密码
 
-//查询图书所用变量
+// 查询图书所用变量
 char type1[MAXLENGTH];
 char bookclass[MAXLENGTH];
 char author[MAXLENGTH];
@@ -34,13 +34,13 @@ char publish[MAXLENGTH];
 char location[MAXLENGTH];
 char info[MAXLENGTH];
 char writer[MAXLENGTH];
-double priceUser;//用户输入的价格
-int locationUser;//用户输入的书架位置
+double priceUser;// 用户输入的价格
+int locationUser;// 用户输入的书架位置
 
-//针对查询图书进行下一次操作的变量
-char book_id_next[MAXLENGTH];  //用户借书的
+// 针对查询图书进行下一次操作的变量
+char book_id_next[MAXLENGTH];  // 用户借书的
 
-//统计
+// 统计
 char type[MAXLENGTH];
 char type2[MAXLENGTH];
 char bookname1[MAXLENGTH];
@@ -50,7 +50,7 @@ char sexid[MAXLENGTH];
 char borrowtimes[MAXLENGTH];
 char grade[MAXLENGTH];
 
-//查看罚金
+// 查看罚金
 char fines[MAXLENGTH];
 char reader_id1[MAXLENGTH];
 char reader_id2[MAXLENGTH];
@@ -62,17 +62,17 @@ char bookname2[MAXLENGTH];
 
 int main() {
 
-    //part0 设置窗口
+    // part0 设置窗口
     widget();
 
     // 初始化进度
     progressBar();
 
-    //part1	初始化数据库
+    // part1	初始化数据库
     mysql_init(&mysql);
-    // progressBar(); //模拟初始化进度
+    // progressBar(); // 模拟初始化进度
 
-    //第一部分：登陆+注册
+    // 第一部分：登陆+注册
     int identity = 0;
 
     while (1) {
@@ -81,7 +81,7 @@ int main() {
         char input = getchar();
         // usleep(500);
         if (input == '1') {
-            puts("\033[2J"); // 清屏操作
+            puts("\033[2J");
             if (readerLogin() != 0) {
                 // usleep(800)
                 continue;
@@ -89,19 +89,19 @@ int main() {
             identity = 1;
             break;
         } else if (input == '2') {
-            puts("\033[2J"); // 清屏操作
+            puts("\033[2J");
             if (adminLogin() != 0) {
                 continue;
             }
             identity = 2;
             break;
         } else if (input == '3') {
-            puts("\033[2J"); // 清屏操作
+            puts("\033[2J");
             if (registerAccount() == 0) {
                 continue;
             }
         } else if (input == '4') {
-            puts("\033[2J"); // 清屏操作
+            puts("\033[2J");
             printf("感谢您的使用!");
             exit(0);
         } else {
@@ -114,12 +114,12 @@ int main() {
     // 读者菜单部分
     if (identity == 1) {
         while (1) {
-            puts("\033[2J"); // 清屏操作
+            puts("\033[2J");
             readerMenu();
             char input = getchar();
             // 借书
             if (input == '1') {
-                puts("\033[2J"); // 清屏操作
+                puts("\033[2J");
                 printf("\n\n\t\t\t将跳转到图书查询菜单，并选择下一步的操作\n\t\t\t");
                 getchar();
                 if (Checkbook(1) == 0) {
@@ -128,16 +128,16 @@ int main() {
                     borrowBooks(book_id_next, login_id);
                     continue;
                 }
-            } else if (input == '2') {  //查询图书信息并还书
-                puts("\033[2J"); // 清屏操作
+            } else if (input == '2') {  // 查询图书信息并还书
+                puts("\033[2J");
                 if (checkBorrowed() == 0) {
                     continue;
                 } else {
                     returnBooks(checkBorrowed_ret, login_id);
                     continue;
                 }
-            } else if (input == '3') { //查询个人信息并修改
-                puts("\033[2J"); // 清屏操作
+            } else if (input == '3') { // 查询个人信息并修改
+                puts("\033[2J");
                 if (checkInfo() == 1) {
                     getchar();
                     continue;
@@ -145,55 +145,54 @@ int main() {
                     updateInfo(updateNum, login_id);
                     continue;
                 }
-                continue;
-            } else if (input == '4') { //申请续借
-                puts("\033[2J"); // 清屏操作
+            } else if (input == '4') { // 申请续借
+                puts("\033[2J");
                 if (checkRenew() == 1) {
                     continue;
                 } else {
                     renew(checkBorrowed_ret, login_id);
                     continue;
                 }
-            } else if (input == '5') { //交罚金
-                puts("\033[2J"); // 清屏操作
+            } else if (input == '5') { // 交罚金
+                puts("\033[2J");
                 payDelayPenalty(login_id);
                 continue;
-            } else if (input == '0') { //退出
-                puts("\033[2J"); // 清屏操作
+            } else if (input == '0') { // 退出
+                puts("\033[2J");
                 printf("感谢您的使用！");
                 exit(0);
             } else {
                 continue;
             }
         }
-    } else { //管理员菜单
+    } else { // 管理员菜单
         while (1) {
-            puts("\033[2J"); // 清屏操作
+            puts("\033[2J");
             adminMenu();
             char input = getchar();
             usleep(500);
             if (input == '1') {
-                puts("\033[2J"); // 清屏操作
+                puts("\033[2J");
                 SecPi();
                 continue;
             } else if (input == '2') {
-                puts("\033[2J"); // 清屏操作
+                puts("\033[2J");
                 ModifyPi();
                 continue;
             } else if (input == '3') {
-                puts("\033[2J"); // 清屏操作
+                puts("\033[2J");
                 Operatebook();
                 continue;
             } else if (input == '4') {
-                puts("\033[2J"); // 清屏操作
+                puts("\033[2J");
                 borrowinfo();
                 continue;
             } else if (input == '5') {
-                puts("\033[2J"); // 清屏操作
+                puts("\033[2J");
                 checkfines();
                 continue;
             } else if (input == '6') {
-                puts("\033[2J"); // 清屏操作
+                puts("\033[2J");
                 countlist();
                 continue;
             } else if (input == '0') {
